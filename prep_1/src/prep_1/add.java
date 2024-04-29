4d96d76b560051a6099c849dc9d515ac3e316c11
Senford Fernandes <senfordfernandes201@gmail.com>
2024-04-29 10:22:42 +0530

    zxcvb

---
+++ prep_1/src/prep_1/add.java
@@ -0,0 +1,15 @@
+package prep_1;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=1;
+		int b=2;
+		int c=0;
+		System.out.println(" "+c);
+
+	}
+
+}
